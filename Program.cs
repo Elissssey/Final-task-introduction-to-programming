f23649ed5ac8f3690e6e0db536fb1848dd7c85c4
EDK <eliseev.rds@gmail.com>
2023-03-25 11:34:40 +0300

    Начало работы над программой и обозначение m

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
 ﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
